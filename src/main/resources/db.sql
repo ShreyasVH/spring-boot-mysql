a58f837c02a56ef6284ef5bd4258208c0a0f201a
Shreyas Hande <shreyas@everwell.org>
2022-12-03 00:29:15 +0530

    initial commit

---
+++ src/main/resources/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `book_store`.`books`
+(
+    `id` BIGINT NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(25) NOT NULL ,
+     `author` VARCHAR(25) NOT NULL ,
+     PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
